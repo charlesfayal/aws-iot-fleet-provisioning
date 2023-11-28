e5a155fddaa805e6f715f0c79c6934742970e5cf
Lena Kråkevik Haraldseid <35926103+Lenakh97@users.noreply.github.com>
2023-11-28 16:21:14 +0100

    test: replace jest with Node.js test runner (#4)
    Fixes #3 
    
    Co-authored-by: Markus Tacker <Markus.Tacker@NordicSemi.no>

--- cdk/helpers/lambdas/commonParent.spec.ts
+++ cdk/helpers/lambdas/commonParent.spec.ts
@@ -1,24 +1,29 @@
 import { commonParent } from './commonParent.js'
+import assert from 'node:assert/strict'
+import { describe, test as it } from 'node:test'
 
-describe('commonParent()', () => {
-	it('should return the common parent directory', () =>
-		expect(
+void describe('commonParent()', () => {
+	void it('should return the common parent directory', () =>
+		assert.equal(
 			commonParent([
 				'/some/dir/lambda/onMessage.ts',
 				'/some/dir/lambda/notifyClients.ts',
 				'/some/dir/lambda/wirepasPublish.ts',
 				'/some/dir/wirepas-5g-mesh-gateway/protobuf/ts/data_message.ts',
 			]),
-		).toEqual('/some/dir/'))
-	it('should return the entire parent tree for a single file', () =>
-		expect(commonParent(['/some/dir/lambda/onMessage.ts'])).toEqual(
+			'/some/dir/',
+		))
+	void it('should return the entire parent tree for a single file', () =>
+		assert.equal(
+			commonParent(['/some/dir/lambda/onMessage.ts']),
 			'/some/dir/lambda/',
 		))
-	it('should return "/" if files have no common directory', () =>
-		expect(
+	void it('should return "/" if files have no common directory', () =>
+		assert.equal(
 			commonParent([
 				'/some/dir/lambda/onMessage.ts',
 				'/other/dir/lambda/onMessage.ts',
 			]),
-		).toEqual('/'))
+			'/',
+		))
 })
